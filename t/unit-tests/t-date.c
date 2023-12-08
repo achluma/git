7bd61622b889d45542ce61fbaa1189b2d34d096f
Achu Luma <ach.lumap@gmail.com>
2023-12-08 02:02:18 +0100

    Port helper/test-date.c to unit-tests/t-date.c
    This commit renames the file `t/helper/test-date.c` to
    `t/unit-tests/t-date.c` to adhere to the project's file structure and
    naming conventions. The file has been updated with the necessary changes
    to maintain its functionality after the renaming.
    
    Furthermore, the code in `t-date.c` has been refactored to utilize the
    new unit testing framework and conventions followed in the project. The
    tests have been rewritten using the testing macros provided by the
    framework, such as `TEST()` and `check_*()`, to improve test
    organization and execution.
    
    Tests from `t/helper/test-date.c` have been migrated to
    `t/unit-tests/t-date.c` using the new framework, ensuring that the
    code functionality remains intact. The test results are now presented
    in a standardized format, facilitating easier analysis and reporting.
    
    In addition to the file renaming, related references in the build
    scripts have been updated to reflect the new file location and name.
    
    This commit does not introduce any functional changes to the code logic
    itself, only the file name, file path, and the testing framework have
    been updated.
    
    Signed-off-by: Achu Luma <ach.lumap@gmail.com>

---
+++ t/unit-tests/t-date.c
@@ -0,0 +1,96 @@
+#include "test-lib.h"
+#include "date.h"
+#include "trace.h"
+
+static void test_show_relative_dates(void)
+{
+	const char *dates[] = {"0", "100", NULL};
+
+	for (const char **argv = dates; *argv; argv++) {
+		struct strbuf buf = STRBUF_INIT;
+		time_t t = atoi(*argv);
+		show_date_relative(t, &buf);
+		check_str(*argv, buf.buf);
+		strbuf_release(&buf);
+	}
+}
+
+static void test_show_human_dates(void)
+{
+	const char *dates[] = {"0", "100", NULL};
+
+	for (const char **argv = dates; *argv; argv++) {
+		time_t t = atoi(*argv);
+		const char *output = show_date(t, 0, DATE_MODE(HUMAN));
+		check_str(*argv, output);
+		free((void *)output);
+	}
+}
+
+static void test_show_dates(void) {
+	const char *dates[] = {"0", "100", NULL};
+	const char *format = "iso";
+
+	for (const char **argv = dates; *argv; argv++) {
+		struct date_mode mode = DATE_MODE_INIT;
+		char *arg;
+		timestamp_t t;
+		int tz;
+		const char *output;
+
+		parse_date_format(format, &mode);
+
+		t = parse_timestamp(*argv, &arg, 10);
+		while (*arg == ' ')
+			arg++;
+		tz = atoi(arg);
+
+		output = show_date(t, tz, &mode);
+		check_str(*argv, output);
+		free((void *)output);
+
+		date_mode_release(&mode);
+	}
+}
+
+static void test_parse_dates(void) {
+	const char *dates[] = {"invalid_date", "2023-10-17 10:00:00 +0200", NULL};
+
+	for (const char **argv = dates; *argv; argv++) {
+		struct strbuf result = STRBUF_INIT;
+		timestamp_t t;
+		int tz;
+		const char *output;
+
+		parse_date(*argv, &result);
+		if (sscanf(result.buf, "%" PRItime " %d", &t, &tz) == 2) {
+			output = show_date(t, tz, DATE_MODE(ISO8601));
+			check_str(*argv, output);
+			free((void *)output);
+		} else {
+			check_str(*argv, "bad");
+		}
+
+		strbuf_release(&result);
+	}
+}
+
+static void test_parse_approxidate(void) {
+	const char *dates[] = {"yesterday", "next week", NULL};
+
+	for (const char **argv = dates; *argv; argv++) {
+		timestamp_t t = approxidate(*argv);
+		const char *output = show_date(t, 0, DATE_MODE(ISO8601));
+		check_str(*argv, output);
+		free((void *)output);
+	}
+}
+
+int cmd_main(int argc, const char **argv) {
+	TEST(test_show_relative_dates(), "Test show_date_relative");
+	TEST(test_show_human_dates(), "Test show_human_dates");
+	TEST(test_show_dates(), "Test show_dates");
+	TEST(test_parse_dates(), "Test parse_dates");
+	TEST(test_parse_approxidate(), "Test parse_approxidate");
+	return test_done();
+}

cd57263dbfed90cf9d9f0911a7de550a6f3b9878
Achu Luma <ach.lumap@gmail.com>
2023-12-12 07:22:11 +0100

    add fxns from test-date.c

--- t/unit-tests/t-date.c
+++ t/unit-tests/t-date.c
@@ -1,64 +1,97 @@
 #include "test-lib.h"
 #include "date.h"
+#include "trace.h"
 
-static void test_show_relative_dates(void) {
-    const char *dates[] = { "0", "100", NULL };
+static void test_show_relative_dates(void)
+{
+    const char *dates[] = {"0", "100", NULL};
 
     for (const char **argv = dates; *argv; argv++) {
-        check_int(show_relative_dates((const char *[]){ *argv, NULL }), ==, 0);
+        struct strbuf buf = STRBUF_INIT;
+        time_t t = atoi(*argv);
+        show_date_relative(t, &buf);
+        check_str(*argv, buf.buf);
+        strbuf_release(&buf);
     }
 }
 
-static void test_show_human_dates(void) {
-    const char *dates[] = { "0", "100", NULL };
+static void test_show_human_dates(void)
+{
+    const char *dates[] = {"0", "100", NULL};
 
     for (const char **argv = dates; *argv; argv++) {
-        check_int(show_human_dates((const char *[]){ *argv, NULL }), ==, 0);
+        time_t t = atoi(*argv);
+        const char *output = show_date(t, 0, DATE_MODE(HUMAN));
+        check_str(*argv, output);
+        free((void *)output);
     }
 }
 
 static void test_show_dates(void) {
-    const char *dates[] = { "0", "100", NULL };
+    const char *dates[] = {"0", "100", NULL};
     const char *format = "iso";
 
     for (const char **argv = dates; *argv; argv++) {
-        check_int(show_dates((const char *[]){ *argv, NULL }, format), ==, 0);
+        struct date_mode mode = DATE_MODE_INIT;
+        char *arg;
+        timestamp_t t;
+        int tz;
+        const char *output;
+
+        parse_date_format(format, &mode);
+
+        t = parse_timestamp(*argv, &arg, 10);
+        while (*arg == ' ')
+            arg++;
+        tz = atoi(arg);
+
+        output = show_date(t, tz, &mode);
+        check_str(*argv, output);
+        free((void *)output);
+
+        date_mode_release(&mode);
     }
 }
 
 static void test_parse_dates(void) {
-    const char *dates[] = { "invalid_date", "2023-10-17 10:00:00 +0200", NULL };
+    const char *dates[] = {"invalid_date", "2023-10-17 10:00:00 +0200", NULL};
 
     for (const char **argv = dates; *argv; argv++) {
-        check_int(parse_dates((const char *[]){ *argv, NULL }), ==, 0);
+        struct strbuf result = STRBUF_INIT;
+        timestamp_t t;
+        int tz;
+	const char *output;
+
+        parse_date(*argv, &result);
+        if (sscanf(result.buf, "%" PRItime " %d", &t, &tz) == 2) {
+            output = show_date(t, tz, DATE_MODE(ISO8601));
+            check_str(*argv, output);
+            free((void *)output);
+        } else {
+            check_str(*argv, "bad");
+        }
+
+        strbuf_release(&result);
     }
 }
 
 static void test_parse_approxidate(void) {
-    const char *dates[] = { "yesterday", "next week", NULL };
+    const char *dates[] = {"yesterday", "next week", NULL};
 
     for (const char **argv = dates; *argv; argv++) {
-        check_int(parse_approxidate((const char *[]){ *argv, NULL }), ==, 0);
+        timestamp_t t = approxidate(*argv);
+        const char *output = show_date(t, 0, DATE_MODE(ISO8601));
+        check_str(*argv, output);
+        free((void *)output);
     }
 }
 
-static void test_parse_approx_timestamp(void) {
-    const char *dates[] = { "yesterday", "next week", NULL };
+int cmd_main(int argc, const char **argv) {
 
-    for (const char **argv = dates; *argv; argv++) {
-        check_int(parse_approx_timestamp((const char *[]){ *argv, NULL }), ==, 0);
-    }
-}
-
-int main(int argc UNUSED, const char **argv UNUSED) {
-    test_start("date unit tests");
-
-    TEST(test_show_relative_dates, "Test show_date_relative");
-    TEST(test_show_human_dates, "Test show_date in human-readable format");
-    TEST(test_show_dates, "Test show_date with custom format");
-    TEST(test_parse_dates, "Test date parsing");
-    TEST(test_parse_approxidate, "Test approxidate");
-    TEST(test_parse_approx_timestamp, "Test approxidate for timestamps");
-
-    return test_finish();
+TEST(test_show_relative_dates(), "Test show_date_relative");
+TEST(test_show_human_dates(), "Test show_human_dates");
+TEST(test_show_dates(), "Test show_dates");
+TEST(test_parse_dates(), "Test parse_dates");
+TEST(test_parse_approxidate(), "Test parse_approxidate");
+    return test_done();;
 }
